292e37e60bd417dc9c38ff04787eab2c5f0dc7db
EBliujus <ev.bliujus@gmail.com>
2023-01-24 16:06:33 +0200

    convert into string

---
+++ Tasks/05-convert.js
@@ -0,0 +1,14 @@
+console.clear()
+
+
+
+function numberToString(num) {
+    return num.toString();
+  }
+
+
+  console.log(numberToString(67),'-->', '67');
+  console.log(numberToString(34),'-->', '34');
+  console.log(numberToString(456),'-->', '456');
+  console.log(numberToString(000),'-->', '000');
+  console.log(numberToString(9877),'-->', '9877');
