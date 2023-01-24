31a5f0d1bcb15396d0550306655b914f0fed7562
EBliujus <ev.bliujus@gmail.com>
2023-01-24 16:38:15 +0200

    dealing with switch

---
+++ Tasks/06-switch.js
@@ -0,0 +1,19 @@
+console.clear()
+const number = 0;
+
+function switchItUp(number)
+ 
+
+switch (number) {
+   if (Input.number) 
+}
+return 'one';
+
+
+console.log(switchItUp(1),"One");
+console.log(switchItUp(3),"Three");
+console.log(switchItUp(2),"Two");
+console.log(switchItUp(4),"Four");
+console.log(switchItUp(6),"Six");
+console.log(switchItUp(10),"Ten");
+console.log(switchItUp(11),"Eleven");
