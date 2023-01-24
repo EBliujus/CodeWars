183349ea2d9198fc61e99c1446f33a0219d877f9
EBliujus <ev.bliujus@gmail.com>
2023-01-24 22:31:31 +0200

    math.abs example

--- Tasks/11-cubanoids.js
+++ Tasks/11-cubanoids.js
@@ -7,3 +7,16 @@ console.log(findDifference([9, 7, 2], [5, 2, 2]), 106);
 console.log(findDifference([11, 2, 5], [1, 10, 8]), 30);
 console.log(findDifference([4, 4, 7], [3, 9, 3]), 31);
 console.log(findDifference([15, 20, 25], [10, 30, 25]), 0);
+
+/* function difference(a, b) {
+  return Math.abs(a - b);
+}
+
+console.log(difference(3, 5));
+// Expected output: 2
+
+console.log(difference(5, 3));
+// Expected output: 2
+
+console.log(difference(1.23456, 7.89012));
+// Expected output: 6.6555599999999995 */
