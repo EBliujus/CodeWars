6f30e61ddb3f9d6d30d57c1a554311ba6c707032
EBliujus <ev.bliujus@gmail.com>
2023-01-29 00:39:10 +0200

    oddSum

---
+++ Tasks/19-oddSum.js
@@ -0,0 +1,13 @@
+
+
+function rowSumOddNumbers(n) {
+        return n ** 3;
+}
+
+
+
+
+console.log(rowSumOddNumbers(1), 1);
+console.log(rowSumOddNumbers(33), 35937);
+console.log(rowSumOddNumbers(5), 125);
+console.log(rowSumOddNumbers(42), 74088);
